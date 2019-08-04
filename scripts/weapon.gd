f5eac071383674d7f89b119bcb99ad7159bc1b34
Milan Gruner <milangruner@gmail.com>
2019-08-04 14:09:27 +0200

    Turn on light when weapon is hovered and play attack animation in attack()

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -4,6 +4,9 @@ export(float, 0, 1) var damage = 1
 export(float, 0, 100) var max_damage_force = 1 # Newton
 
 onready var model = $MeshInstance
+onready var light = $OmniLight
+onready var animation = $AnimationPlayer
+
 var is_attached = false
 var force = Vector3()
 
@@ -24,7 +27,7 @@ func take_damage(damage, source):
 	pass
 
 func attack():
-	pass
+	animation.play("attack")
 
 func get_damage():
 	var force_scale = clamp(force.length() / max_damage_force, 0, 1)
@@ -45,7 +48,7 @@ func _on_input_event(camera, event, click_position, click_normal, shape_idx):
 	pass
 
 func _on_mouse_entered():
-	pass
+	light.light_energy = 5
 
 func _on_mouse_exited():
-	pass
+	light.light_energy = 0
