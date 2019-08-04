be4ca47179d069d8c230ae9e56f166bd1835a00a
Milan Gruner <milangruner@gmail.com>
2019-08-04 13:28:28 +0200

    Create CollisionShape for scythe and attach RigidBody events to weapon.gd

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -19,10 +19,22 @@ func get_damage():
 	var force_scale = clamp(force.length() / max_damage_force, 0, 1)
 	return force_scale * damage
 
-func _body_entered(body: PhysicsBody):
+func _on_body_entered(body: PhysicsBody):
 	print("Body entered", body)
 	var damage = get_damage()
 	
 	if body.has_method("take_damage"):
 		print("Body taking damage: ", damage)
 		body.take_damage(damage, self)
+
+func _on_body_exited(body):
+	pass
+
+func _on_input_event(camera, event, click_position, click_normal, shape_idx):
+	pass
+
+func _on_mouse_entered():
+	pass
+
+func _on_mouse_exited():
+	pass
