296380812c81a644fd09febe9151a63e2646a162
Milan Gruner <milangruner@gmail.com>
2019-08-06 00:49:01 +0200

    Create UI scene and script and connect button events to it

---
+++ scripts/ui.gd
@@ -0,0 +1,16 @@
+extends Control
+
+signal attack
+signal move
+
+func _ready():
+	pass
+
+func _on_pause_toggled(button_pressed):
+	pass
+
+func _on_attack_button_down():
+	pass
+
+func _on_attack_button_up():
+	pass
